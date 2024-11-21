e9317691ea35d7f5edfafc1418e9d6e27575a18d
TongZhengHong <zhtong315@gmail.com>
2024-11-21 00:31:23 -0500

    Bundle andorid app (AAB)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
 import AppBar from './components/AppBar';
 import BottomAppBar from './components/BottomNavBar';
-import {openExternalFile} from './utils/FileUtils';
-import NativeFilterModule from '../specs/NativeFilterModule';
 
 function App(): React.JSX.Element {
-  const openFile = async () => {
-    openExternalFile();
-  };
-
-  useEffect(() => {
-    // openFile();
-    const data = Array.from({ length: 100 }, () => Math.random() * 100);
-    const array = NativeFilterModule.notchFilter(data, 1000.0, 50.0, 10.0);
-    console.log(array);
-  });
-
   return (
     <SafeAreaView style={styles.container}>
       <AppBar />
